8fca5ad336a65b3aa4e6fc269e73873fc08e5049
neha9029 <nehanaik9029@gmail.com>
2025-08-29 21:59:03 -0600

    repo name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import preact from "@preact/preset-vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
-  base: "/<color-palette-theme>/",
+  base: "/color-palette-theme/",
 });
