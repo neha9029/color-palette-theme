b8179912dfd30e4ab464ea7b0ed24c4dfe6a451d
neha9029 <nehanaik9029@gmail.com>
2025-08-29 22:14:46 -0600

    added hasrouter

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Theme from "./pages/Theme";
 import Mood from "./pages/Mood";
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route index element={<Homepage />} />
-        <Route path="theme" element={<Theme />} />
-        <Route path="mood" element={<Mood />} />
+        <Route path="/theme" element={<Theme />} />
+        <Route path="/mood" element={<Mood />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
